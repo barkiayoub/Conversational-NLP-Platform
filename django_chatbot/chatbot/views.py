0fd4237122bb3a1430c2de7b8b3454956b41727e
barkiayoub <ayoubbarki17@gmail.com>
2025-01-16 14:41:17 +0100

    API Integrated

--- django_chatbot/chatbot/views.py
+++ django_chatbot/chatbot/views.py
@@ -1,67 +1,20 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# import openai
-
-#
-# openai.api_key = openai_api_key
-
-# def ask_openai(message):
-#     response = openai.Completion.create(
-#         model= "gpt-3.5-turbo",
-#         prompt = message,
-#         max_tokens=150,
-#         n=1,
-#         stop=None,
-#         temperature=0.7,
-#     )
-#     # print(response)
-#     answer = response.choices[0].text.strip()
-#     return
-
-
-# # Create your views here.
-# def chatbot(request):
-#     if request.method == 'POST':
-#         message = request.POST.get('message')
-#         response = ask_openai(message)
-#         return JsonResponse({'message': message,'response': response})
-#     return render(request, 'chatbot.html')
-
-
 from django.shortcuts import render
 from django.http import JsonResponse
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import os
-from huggingface_hub import login
+from langchain import HuggingFaceHub
 
-# Load the Mistral model and tokenizer from Hugging Face
-login("")
-
-model_name = "mistralai/Mistral-7B-Instruct-v0.3"  # Replace with the correct model name on Hugging Face
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-def ask_mistral(message):
-    # Tokenize the input message
-    inputs = tokenizer.encode(message, return_tensors="pt")
-
-    # Generate a response
-    outputs = model.generate(
-        inputs,
-        max_length=150,
-        temperature=0.7,
-        num_return_sequences=1,
-        do_sample=True
-    )
-
-    # Decode and return the generated text
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.3",
+                     model_kwargs={
+                          "max_length": 1000,  # Maximum length of the generated sequence
+                          "max_new_tokens": 10000,  # Maximum number of new tokens to generate
+                     },
+                     huggingfacehub_api_token="replace_with_your_api_key"
+                    )
 
 # Create your views here.
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        # response = ask_mistral(message)
-        response = ask_mistral(message)
+        response = llm.invoke(message)
+        print(response)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
